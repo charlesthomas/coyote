f8b625b4d8f3efa959837eb7907cfb00e2825b99
Charles Thomas <ch@rlesthom.as>
2015-12-08 14:14:52 -0500

    read celery config if it exists and is configured in the coyote config

--- coyote/celery.py
+++ coyote/celery.py
@@ -6,4 +6,9 @@ from .utils.conf_builder import AppConfig
 coyote = AppConfig('config.yaml')
 sys.path += coyote.syspaths
 app = Celery(include=coyote.includes)
+
+# assume this shouldn't be trapped by coyote.halt_on_init_error = False
+if coyote.celery_config is not None:
+    app.config_from_object(coyote.celery_config)
+
 app.conf.update(CELERYBEAT_SCHEDULE=coyote.schedules)
